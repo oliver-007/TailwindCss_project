7ebb7215128f3b7093ed0148fcd5745f73fe6b32
Oliver <oliverahmed000@gmail.com>
2022-01-09 11:03:28 +0600

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    screens: {
+      xs: "480PX",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      fontFamily: {
+        primary: "Poppins, sans-serif",
+        sans: "Roboto, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
